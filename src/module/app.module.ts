16f97cd180ad2a8e922a45bdee0cc7567f2a7002
icepoint0 <351711778@qq.com>
2016-11-16 18:25:29 +0800

    fix bug

--- src/module/app.module.ts
+++ src/module/app.module.ts
@@ -5,10 +5,12 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }  from '../components/app';
 import {routing} from "../routes/app.routes";
+import {HomeComponent} from  '../app/welcome/home.component'
+import {PageNotFoundComponent} from "../components/404";
 
 @NgModule({
     imports: [BrowserModule ,routing],
-    declarations: [ AppComponent ,HomeComponent],
+    declarations: [ AppComponent ,HomeComponent,PageNotFoundComponent],
     bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
